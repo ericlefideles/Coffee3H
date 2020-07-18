ab7eb722a13647e99f4f0cd38eba2cfeed10d905
Ericles Fideles <ericles.fideles@eletraenergy.com>
2020-07-18 15:15:56 -0300

    first commit

---
+++ Coffee/Data/AplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coffee.Data
+{
+    public class AplicationDbContext : IdentityDbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Coffee.Models.Contact> Contact { get; set; }
+    }
+}
---
+++ Coffee/Models/ContactModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coffee.Models
+{
+    public class Contact
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [DisplayName("Nome")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [DisplayName("Telefone")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(11, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string CellPhone { get; set; }
+
+        [DisplayName("Email")]
+        [StringLength(150, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Email { get; set; }
+
+        [DisplayName("Comentários")]
+        public string Comments { get; set; }
+
+        public DateTime DateCreate { get; set; }
+
+
+    }
+}
